ae625e31cd068e580fd387b6b3e10d80f9b05eb0
ytosati <ytosati@gmail.com>
2025-06-16 10:41:49 -0300

    Inicializando projeto

---
+++ PokeAPI/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PokeAPI',
+  webDir: 'www'
+};
+
+export default config;
